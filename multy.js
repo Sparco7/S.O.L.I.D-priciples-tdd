deb61b96f7a3f220ed51878fff5b01315547f9c3
Barak Ben Gur <barakbbg@gmail.com>
2021-08-12 18:53:27 +0300

    adding multyply test

---
+++ multy.js
@@ -0,0 +1,6 @@
+function multy(a, b){
+    return a * b
+}
+
+
+export default multy
---
+++ multy.test.js
@@ -0,0 +1,6 @@
+import multy from './multy'
+// const multy  =  require('./multy')
+
+test('multiplies 3 + 2 expects to be 6', () =>{
+    expect(multy(3,2)).toBe(6)
+})
