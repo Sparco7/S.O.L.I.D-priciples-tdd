30e32a8a65471b356cfaa0c6940c3ade06bbc807
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-08-12 16:33:30 +0300

    Added test to fail

--- shapes.test.js
+++ shapes.test.js
@@ -6,3 +6,8 @@ test('show return the length of a square to 5', () =>{
     const sq1 = new Square(5)
     expect(sq1.getLegth()).toBe(5)
 });
+
+test('show return the length of a square to 5', () =>{
+    const sq1 = new Square(5)
+    expect(sq1.length).toBe(7)
+});
